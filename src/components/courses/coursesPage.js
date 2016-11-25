aacf1a7c55479eed4bae82cd9cdfbe1427769d1c
Neil <neil@snapzio.com>
2016-11-25 16:24:40 +0800

    added event

--- src/components/courses/coursesPage.js
+++ src/components/courses/coursesPage.js
@@ -1,13 +1,55 @@
-import React, {ProtoTypes} from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseAction from './action/courseAction';
 
 class Courses extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: {title: null}
+    };
+
+    // THIS THE RECOMMENDED WAY TO BIND AN ACTION TO AVOID PERFORMANCE ISSUE
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    // alert(`Success: ${this.state.course.title}`);
+    this.props.dispatch(courseAction.createCourse(this.state.course));
+  }
+
+  courseList(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
     return (
       <div>
         <h1>Courses Page</h1>
+        {this.props.courses.map(this.courseList)}
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
+        <input type="submit" onClick={this.onClickSave} value="Save" />
       </div>
     );
   }
 }
 
-export default Courses;
+Courses.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(Courses);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import configureStore from './store/reduxStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
 
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById("app")
 );
